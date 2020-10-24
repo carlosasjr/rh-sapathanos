6b2556ad837bfa8324b5c7c88413b6ead711a927
Carlos Junior <carlos@theplace.com.br>
2020-10-24 11:54:31 -0300

    Criação das migrations Employee e Documents

--- database/migrations/2020_10_24_112305_create_employees_table.php
+++ database/migrations/2020_10_24_112305_create_employees_table.php
@@ -15,6 +15,53 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('fantasy');
+            $table->string('email')->nullable();
+
+            $table->string('image')->nullable();
+
+            $table->string('cpf')->nullable();
+            $table->string('rg')->nullable();
+            $table->date('date_birth')->nullable();
+
+            $table->string('cellphone')->nullable();
+            $table->string('telephone')->nullable();
+
+            $table->string('partner')->nullable();
+            $table->enum('marital_status', ['Solteiro', 'Casado', 'Separado', 'Divorciado', 'Viúvo'])->nullable();
+            $table->string('father')->nullable();
+            $table->string('mother')->nullable();
+            $table->string('naturalness')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('office')->nullable();
+
+            $table->string('street')->nullable();
+            $table->string('number')->nullable();
+            $table->string('district')->nullable();
+            $table->string('complement')->nullable();
+            $table->string('cep')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+
+            $table->string('salary')->nullable();
+            $table->time('hours')->nullable();
+
+
+            $table->text('observation')->nullable();
+            $table->enum('status', ['A', 'I'])->default('A')->comment('A -> Ativo, I -> Inativo');
+
+
+            $table->bigInteger('user_id')->unsigned();
+
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2020_10_24_114728_create_employee_documents_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_documents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->string('description');
+            $table->string('file');
+
+            $table->foreign('employee_id')
+                  ->references('id')
+                  ->on('employees')
+                  ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_documents');
+    }
+}
